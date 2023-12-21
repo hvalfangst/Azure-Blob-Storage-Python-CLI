cf18f46f743a73c92b0964d8b9b281158fb71bb3
hvalfangst <hvalfangst@tidehunter.eu>
2023-12-21 23:37:29 +0100

    Refactored code

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def create_container(container_name):
     blob_service_client = get_blob_service_client()
     container_client = blob_service_client.get_container_client(container_name)
     container_client.create_container()
+    print(f"Container with name '{container_name}' has been created.\n")
 
 
 def list_containers(print_containers: bool) -> list:
@@ -23,6 +24,7 @@ def list_containers(print_containers: bool) -> list:
     else:
         container_names = [container.name for container in containers]
         if print_containers:
+            print("CONTAINERS")
             print("------------------------------------------------")
             for i, container_name in enumerate(container_names):
                 print(f"[{i + 1}] {container_name}")
@@ -31,214 +33,181 @@ def list_containers(print_containers: bool) -> list:
             return container_names
 
 
-def delete_container():
+def choose_container():
     container_names = list_containers(False)
 
-    if not container_names:
-        print("No containers have been created.")
-    else:
-        print("------------------------------------------------")
-        for i, container_name in enumerate(container_names):
-            print(f"[{i + 1}] {container_name}")
-        print("------------------------------------------------")
-
-        container_index = int(input("Choose Container: \n\n"))
-        container_index -= 1
-
-        if 0 <= container_index < len(container_names):
-            selected_container = container_names[container_index]
-            blob_service_client = get_blob_service_client()
-            container_client = blob_service_client.get_container_client(selected_container)
-            container_client.delete_container()
-        else:
-            print("Invalid Container index.\n\n")
-
-
-def upload_blob():
-    container_names = list_containers(False)
     if not container_names:
         print("No containers have been created.\n\n")
+        return None
     else:
+        print("CONTAINERS")
         print("------------------------------------------------")
         for i, container_name in enumerate(container_names):
             print(f"[{i + 1}] {container_name}")
         print("------------------------------------------------")
 
         container_index = int(input("Choose container: \n\n"))
-
         container_index -= 1
 
         if 0 <= container_index < len(container_names):
-            selected_container = container_names[container_index]
-            blob_name = input("Enter blob name: \n\n")
-            source_file_path = input("Enter source file path: \n\n")
-            blob_service_client = get_blob_service_client()
-            container_client = blob_service_client.get_container_client(selected_container)
-
-            with open(source_file_path, "rb") as data:
-                blob_client = container_client.get_blob_client(blob_name)
-                blob_client.upload_blob(data)
+            return container_names[container_index]
         else:
-            print("Invalid index.\n\n")
+            print("Invalid index\n\n")
+            return None
+
+
+def choose_blob():
+    selected_container = choose_container()
+
+    if selected_container:
+        blob_service_client = get_blob_service_client()
+        container_client = blob_service_client.get_container_client(selected_container)
+
+        blobs = list(container_client.list_blobs())
+
+        if not blobs:
+            print(f"No blobs have been created in container [{selected_container}]\n\n")
+            return None, None
+        else:
+            print("BLOBS")
+            print("------------------------------------------------")
+            for i, blob in enumerate(blobs):
+                print(f"[{i + 1}] {blob.name}")
+            print("------------------------------------------------")
+
+            blob_index = int(input("Choose Blob: \n\n"))
+            blob_index -= 1
+
+            if 0 <= blob_index < len(blobs):
+                selected_blob = blobs[blob_index]
+                return selected_blob, container_client
+            else:
+                print("Invalid Blob index.\n\n")
+                return None, None
+
+
+def delete_container():
+    selected_container = choose_container()
+
+    if selected_container:
+        blob_service_client = get_blob_service_client()
+        container_client = blob_service_client.get_container_client(selected_container)
+        container_client.delete_container()
+        print(f"Container with name '{selected_container}' has been deleted from the storage account.\n")
 
 
 def list_blobs():
-    container_names = list_containers(False)
+    selected_container = choose_container()
 
-    if not container_names:
-        print("No containers have been created.\n\n")
-    else:
-        print("------------------------------------------------")
-        for i, container_name in enumerate(container_names):
-            print(f"[{i + 1}] {container_name}")
-        print("------------------------------------------------")
+    if selected_container:
+        blob_service_client = get_blob_service_client()
+        container_client = blob_service_client.get_container_client(selected_container)
+        blobs = list(container_client.list_blobs())
 
-        container_index = int(input("Choose container: \n\n"))
-
-        container_index -= 1
-
-        if 0 <= container_index < len(container_names):
-            selected_container = container_names[container_index]
-            blob_service_client = get_blob_service_client()
-            container_client = blob_service_client.get_container_client(selected_container)
-
-            blobs = list(container_client.list_blobs())
-
-            if len(blobs) == 0:
-                print(f"No blobs found in container {selected_container}.\n\n")
-            else:
-                print("------------------------------------------------")
-                for i, blob in enumerate(blobs):
-                    print(f"[{i + 1}] {blob.name}")
-                print("------------------------------------------------")
+        if len(blobs) == 0:
+            print(f"No blobs found in container {selected_container}.\n\n")
         else:
-            print("Invalid index\n\n")
+            print("BLOBS")
+            print("------------------------------------------------")
+            for i, blob in enumerate(blobs):
+                print(f"[{i + 1}] {blob.name}")
+            print("------------------------------------------------")
+
+
+def upload_blob():
+    selected_container = choose_container()
+
+    if selected_container:
+        blob_name = input("Enter blob name: \n\n")
+        source_file_path = input("Enter source file path: \n\n")
+        blob_service_client = get_blob_service_client()
+        container_client = blob_service_client.get_container_client(selected_container)
+
+        with open(source_file_path, "rb") as data:
+            blob_client = container_client.get_blob_client(blob_name)
+            blob_client.upload_blob(data)
+            print(f"Blob with content derived from source '{source_file_path}' has been uploaded the "
+                  f"following container: '{selected_container}'")
 
 
 def download_blob(destination_file_path):
-    container_names = list_containers(False)
+    selected_blob, container_client = choose_blob()
 
-    if not container_names:
-        print("No containers have been created.\n\n")
-    else:
-        print("------------------------------------------------")
-        for i, container_name in enumerate(container_names):
-            print(f"[{i + 1}] {container_name}")
-        print("------------------------------------------------")
-
-        container_index = int(input("Choose Container: \n\n"))
-        container_index -= 1
-
-        if 0 <= container_index < len(container_names):
-            selected_container = container_names[container_index]
-            blob_service_client = get_blob_service_client()
-            container_client = blob_service_client.get_container_client(selected_container)
-
-            blobs = list(container_client.list_blobs())
-
-            if not blobs:
-                print(f"No blobs have been created in container [{selected_container}]\n\n")
-            else:
-                print("------------------------------------------------")
-                for i, blob in enumerate(blobs):
-                    print(f"[{i + 1}] {blob.name}")
-                print("------------------------------------------------")
-
-                blob_index = int(input("Choose Blob: \n\n"))
-                blob_index -= 1
-
-                if 0 <= blob_index < len(blobs):
-                    selected_blob = blobs[blob_index]
-                    with open(destination_file_path, "wb") as file:
-                        blob_client = container_client.get_blob_client(selected_blob)
-                        blob_data = blob_client.download_blob()
-                        file.write(blob_data.readall())
-                else:
-                    print("Invalid Blob index.\n\n")
-
-        else:
-            print("Invalid Container index.\n\n")
+    if selected_blob and container_client:
+        with open(destination_file_path, "wb") as file:
+            blob_client = container_client.get_blob_client(selected_blob)
+            blob_data = blob_client.download_blob()
+            file.write(blob_data.readall())
+            print(f"Blob with name '{selected_blob.name}' and size {selected_blob.size} bytes has been downloaded.\n")
 
 
 def delete_blob():
-    container_names = list_containers(False)
+    selected_blob, container_client = choose_blob()
 
-    if not container_names:
-        print("No containers have been created.\n\n")
-    else:
-        print("------------------------------------------------")
-        for i, container_name in enumerate(container_names):
-            print(f"[{i + 1}] {container_name}")
-        print("------------------------------------------------")
-
-        container_index = int(input("Choose Container: \n\n"))
-        container_index -= 1
-
-        if 0 <= container_index < len(container_names):
-            selected_container = container_names[container_index]
-            blob_service_client = get_blob_service_client()
-            container_client = blob_service_client.get_container_client(selected_container)
-
-            blobs = list(container_client.list_blobs())
-
-            if not blobs:
-                print(f"No blobs have been created in container [{selected_container}]\n\n")
-            else:
-                print("------------------------------------------------")
-                for i, blob in enumerate(blobs):
-                    print(f"[{i + 1}] {blob.name}")
-                print("------------------------------------------------")
-
-                blob_index = int(input("Choose Blob: \n\n"))
-                blob_index -= 1
-
-                if 0 <= blob_index < len(blobs):
-                    selected_blob = blobs[blob_index]
-                    blob_client = container_client.get_blob_client(selected_blob)
-                    blob_client.delete_blob()
-                else:
-                    print("Invalid Blob index.\n\n")
-
-        else:
-            print("Invalid Container index.\n\n")
+    if selected_blob and container_client:
+        blob_client = container_client.get_blob_client(selected_blob)
+        blob_client.delete_blob()
+        print(f"Blob with name '{selected_blob.name}' has been deleted.\n")
 
 
 def main():
     validate_connection_string()
+    print_menu()
 
     while True:
-        print_menu()
-
         choice = input("Enter your choice (0-7): \n\n")
 
         if choice == "1":
+            print("\n")
             container_name = input("Enter desired container name: \n\n")
             create_container(container_name)
+            back_to_menu_prompt()
 
         elif choice == "2":
+            print("\n")
             list_containers(True)
+            back_to_menu_prompt()
 
         elif choice == "3":
+            print("\n")
             delete_container()
+            back_to_menu_prompt()
 
         elif choice == "4":
+            print("\n")
             upload_blob()
+            back_to_menu_prompt()
 
         elif choice == "5":
+            print("\n")
             list_blobs()
+            back_to_menu_prompt()
 
         elif choice == "6":
+            print("\n")
             destination_file_path = input("Enter destination file path: \n\n")
             download_blob(destination_file_path)
+            back_to_menu_prompt()
 
         elif choice == "7":
+            print("\n")
             delete_blob()
+            back_to_menu_prompt()
 
         elif choice == "0":
+            print("\n")
             break
 
         else:
             print("Invalid choice. Please enter a number between 0 and 7.\n\n")
+
+
+def back_to_menu_prompt():
+    while True:
+        choice = input("\nBack to menu? Type 'y'\n")
+        if choice == "y":
+            print_menu()
+            break
 
 
 def validate_connection_string():
@@ -250,7 +219,7 @@ def validate_connection_string():
 
 
 def print_menu():
-    print("\nAzure Storage Blobs:")
+    print("\n\n\nAzure Storage Blobs:")
     print("------------------------------------------------------------")
     print("1. Create Container")
     print("2. List Containers")
